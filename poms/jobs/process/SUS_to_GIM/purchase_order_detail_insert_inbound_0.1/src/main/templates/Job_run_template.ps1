1f871b49cd687c71e0f841d76d33854f10c558d6
jran0212 <janaka.ranathunga@syscolabs.com>
2022-10-28 15:48:25 +0530

    updated SLI_MVP2 verification

---
+++ poms/jobs/process/SUS_to_GIM/purchase_order_detail_insert_inbound_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms512M' '-Xmx2048M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.17.1.jar;../lib/log4j-api-2.17.1.jar;../lib/log4j-core-2.17.1.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/jt400-9.8.jar;../lib/postgresql-42.2.14.jar;../lib/crypto-utils-0.31.12.jar;purchase_order_detail_insert_inbound_0_1.jar;' sus_sli_etl.purchase_order_detail_insert_inbound_0_1.Purchase_Order_Detail_Insert_Inbound --context=Default $args
---
+++ poms/jobs/process/SUS_to_GIM/sli_test_inbound_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.17.1.jar;../lib/log4j-api-2.17.1.jar;../lib/log4j-core-2.17.1.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/crypto-utils-0.31.12.jar;sli_test_inbound_0_1.jar;entity_inbound_0_1.jar;' sus_sli_etl.sli_test_inbound_0_1.SLI_Test_Inbound --context=Default $args
